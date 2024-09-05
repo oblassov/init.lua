885ead15e2848f1fbf9dbaed275138d946705660
Artemiy Oblassov <oblassov@protonmail.com>
2024-09-06 02:23:27 +0500

    Personal neovim config

---
+++ lua/config/set.lua
@@ -0,0 +1,52 @@
+vim.opt.guicursor = ""
+--vim.opt.cursorline = true
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOMEPATH") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 9
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
+
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "82"
+
+vim.o.keywordprg = ":help"
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+
+vim.opt.list = true
+vim.opt.listchars:append({
+	multispace = "   .",
+	tab = "· ",
+	lead = "·",
+	leadmultispace = "·   ",
+	trail = "~",
+	extends = ">",
+	precedes = "<",
+	nbsp = "+",
+})
