f649e861147b43c78dc6d3b2d35816c9393d989f
Artemiy Oblassov <oblassov@protonmail.com>
2024-09-22 02:25:25 +0500

    Changed some bindings and moved cmp bindings into lsp.lua

--- lua/config/lazy/harpoon.lua
+++ lua/config/lazy/harpoon.lua
@@ -1,43 +1,49 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 	config = function()
 		local harpoon = require("harpoon")
 
 		harpoon:setup()
 
-		vim.keymap.set("n", "<leader>a", function()
+		vim.keymap.set("n", "<leader>h", function()
 			harpoon:list():add()
 		end)
-		vim.keymap.set("n", "<C-a>", function()
+		vim.keymap.set("n", "<C-h>", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		vim.keymap.set("n", "<C-h>", function()
+		vim.keymap.set("n", "<C-a>", function()
 			harpoon:list():select(1)
 		end)
-		vim.keymap.set("n", "<C-j>", function()
+		vim.keymap.set("n", "<C-s>", function()
 			harpoon:list():select(2)
 		end)
-		vim.keymap.set("n", "<C-k>", function()
+		vim.keymap.set("n", "<C-d>", function()
 			harpoon:list():select(3)
 		end)
-		vim.keymap.set("n", "<C-l>", function()
+		vim.keymap.set("n", "<C-f>", function()
 			harpoon:list():select(4)
 		end)
+		vim.keymap.set("n", "<C-g>", function()
+			harpoon:list():select(5)
+		end)
 
-		vim.keymap.set("n", "<leader><C-h>", function()
+		vim.keymap.set("n", "<leader><C-a>", function()
 			harpoon:list():replace_at(1)
 		end)
-		vim.keymap.set("n", "<leader><C-j>", function()
+		vim.keymap.set("n", "<leader><C-s>", function()
 			harpoon:list():replace_at(2)
 		end)
-		vim.keymap.set("n", "<leader><C-k>", function()
+		vim.keymap.set("n", "<leader><C-d>", function()
 			harpoon:list():replace_at(3)
 		end)
-		vim.keymap.set("n", "<leader><C-l>", function()
+		vim.keymap.set("n", "<leader><C-f>", function()
 			harpoon:list():replace_at(4)
+		end)
+		vim.keymap.set("n", "<leader><C-g>", function()
+			harpoon:list():replace_at(5)
 		end)
 	end,
 }
--- lua/config/lazy/telescope.lua
+++ lua/config/lazy/telescope.lua
@@ -22,5 +22,32 @@ return {
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
 		end)
+
+		local harpoon = require("harpoon")
+		harpoon:setup({})
+
+		-- basic telescope configuration
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
+
+		vim.keymap.set("n", "<leader>ph", function()
+			toggle_telescope(harpoon:list())
+		end, { desc = "Open harpoon window" })
 	end,
 }
